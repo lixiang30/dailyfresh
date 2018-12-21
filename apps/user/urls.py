a3eca8cfb5b68286e565169016f402cd8a6427dc
lixiang30 <534551900@qq.com>
2018-12-21 23:28:58 +0800

    页面模板和子页设计

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url
-from .views import RegisterView,ActiveView,LoginView
+from .views import RegisterView,ActiveView,LoginView,UserInfoView,UserOrderView,AddressView
 
 urlpatterns = [
     # url(r'^register$',views.register,name='register'),
     url(r'^register$',RegisterView.as_view(),name='register'),
     url(r'^active/(?P<token>.*)$',ActiveView.as_view(),name='active'),
-    url(r'^login$',LoginView.as_view(),name='login')
+    url(r'^login$',LoginView.as_view(),name='login'),
+    url(r'^$',UserInfoView.as_view(),name='user'),
+    url(r'^order$',UserOrderView.as_view(),name='order'),
+    url(r'^address$',AddressView.as_view(),name="address"),
 ]
--- apps/user/views.py
+++ apps/user/views.py
@@ -175,3 +175,21 @@ class LoginView(View):
             #用户名或密码错误
             return render(request,'login.html',{"errmsg":"用户名或密码错误"})
         #返回应答
+
+class UserInfoView(View):
+    """用户中心－信息页"""
+    def get(self,request):
+        """显示"""
+        return render(request,"user_center_info.html",{"page":"user"})
+
+class UserOrderView(View):
+    """用户中心－订单页"""
+    def get(self,request):
+        """显示"""
+        return render(request,"user_center_order.html",{"page":"order"})
+
+class AddressView(View):
+    """用户中心－地址页"""
+    def get(self,request):
+        """显示"""
+        return render(request,"user_center_site.html",{"page":"address"})
