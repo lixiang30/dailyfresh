5c506e91116510a3a20d9c40c1a509b4dd52c8b2
lixiang30 <534551900@qq.com>
2018-12-25 22:28:33 +0800

    django配合fdfs上传图片

--- dailyfresh/settings.py
+++ dailyfresh/settings.py
@@ -149,3 +149,12 @@ SESSION_CACHE_ALIAS = "default"
 
 #配置登录URL地址
 LOGIN_URL='/user/login' # 默认/accounts/login,login后面不能多/
+
+# 设置Django的文件存储类
+DEFAULT_FILE_STORAGE='utils.fdfs.storage.FDFSStorage'
+
+# 设置fdfs使用的client.conf文件路径
+FDFS_CLIENT_CONF='./utils/fdfs/client.conf'
+
+# 设置fdfs存储服务器上nginx的IP和端口号
+FDFS_URL='http://127.0.0.1:8888/'
